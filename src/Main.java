f6d0e2d8188513a56c295a52532dc6d7d0f0e0df
Prestone NGAYO <prestonengayo9@gmail.com>
2025-02-22 16:19:12 +0100

    if-elseif-else

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,26 @@
-import java.time.LocalDate;
-import java.util.Date;
 import java.util.Scanner;
-
 
 public class Main {
 
 
     public static void main(String[] args) {
-        //String Exercise
-        String firstname;
-        String lastname;
+        // Structure conditionnelle
 
-        String firstPart = "";
-        String secondPart = "";
 
-        Scanner keyboard = new Scanner(System.in);
+        int age;
+        Scanner clavier = new Scanner(System.in);
 
-        // recuperation du nom et prenom de l'utilisateur
-        System.out.println("Entrer votre prenom : ");
-        firstname = keyboard.nextLine();
-        System.out.println("Entrer votre nom de famille : ");
-        lastname = keyboard.nextLine();
+        System.out.println("veuillez entrer votre âge :");
+        age = clavier.nextInt();
 
-        // mettre le nom en majiscule
-        lastname = lastname.toUpperCase();
+        if(age < 18){
+            System.out.println("Vous êtes encore mineur");
+        }else  if(age > 18 ){
+            System.out.println("Vous êtes majeur");
+        }else{
+            System.out.println("Vous venez juste de devenir majeur !");
+        }
 
-        // recuperation du premier caractere du prenom et transformation de ce caractère en string
-        firstPart = String.valueOf(firstname.charAt(0));
-        firstPart = firstPart.toUpperCase();
-
-        //recupération du reste de prenom et le mettre en miniscule
-        secondPart = firstname.substring(1).toLowerCase();
-
-        //concatener les deux parties du prénom
-        firstname = firstPart.concat(secondPart);
-
-        //affichage
-        System.out.println("Bienvenue "+ firstname + " " + lastname);
 
    }
 
