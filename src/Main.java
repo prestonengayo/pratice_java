0979d34c9ac210b10481dd706ada6bb4821637b8
Prestone NGAYO <prestonengayo9@gmail.com>
2025-02-22 17:13:44 +0100

    for boucle

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,34 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
 
     public static void main(String[] args) {
-        // Structure conditionnelle
+        // Boucle
+        String prenom;
+        final String VOYELLE = "aeuioy";
+        int nombreVoyelle = 0;
+        String voyelletrouve = "";
 
-
-        int heure;
         Scanner clavier = new Scanner(System.in);
 
-        System.out.println("Il est quelle heure ");
-        heure = clavier.nextInt();
+        System.out.println("entrer votre prénom svp : ");
+        prenom = clavier.nextLine().toLowerCase();
 
+        for (int index = 0; index < prenom.length(); index++){
+            char caractere = prenom.charAt(index);
 
-        switch (heure){
-            case 6 : System.out.println("C'est le matin ");
-            break;
-            case 12 : System.out.println("C'est juste le midi !");
-            break;
-            case 18 : System.out.println("C'est le soir");
-            break;
-            default: System.out.println("Cas non prise en charge !");
+            for (int index1 = 0; index1 < VOYELLE.length(); index1 ++){
+                if (caractere == VOYELLE.charAt(index1)){
+                   voyelletrouve = voyelletrouve.concat(String.valueOf(caractere));
+                    nombreVoyelle++;
+                }
+            }
         }
+
+        System.out.println("Votre prenom contient : "+ nombreVoyelle + " voyelle"+ ((nombreVoyelle > 1) ? "s" : ""));
+        System.out.println("Les voyelles trouvées sont : "+ voyelletrouve);
 
    }
 
