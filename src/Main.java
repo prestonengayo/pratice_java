4a84cacd83dc01e0eebddd4f22c0ac6f46162c78
Prestone NGAYO <prestonengayo9@gmail.com>
2025-02-22 18:36:12 +0100

    multidimensional tab

--- src/Main.java
+++ src/Main.java
@@ -7,25 +7,17 @@ public class Main {
     public static void main(String[] args) {
         // tableau
 
+        int[][] nomber = {{2,4,6}, {1,3,5} };
 
-        String[] jourSemaine = new String[7];
+        System.out.println(nomber[0][2]);
 
-        Scanner clavier = new Scanner(System.in);
-        String[] mois = {"Janvier", "fevrier", "Mars", "Avril"};
+        System.out.println(nomber.length);
 
-
-        System.out.println(jourSemaine.length);
-        for(int i = 0; i < jourSemaine.length ; i++){
-            System.out.println("Entrer le jour n°"+ (i+1));
-            jourSemaine[i] = clavier.nextLine();
+        for (int i = 0 ; i < nomber.length; i++){
+            for (int j = 0; j < nomber[i].length; j++){
+                System.out.println("L'enfant se trouvant à la position n°"+(j+1) + " du tableau "+(i+1)+" est : "+nomber[i][j]);
+            }
         }
-
-
-        for (String jour : jourSemaine){
-            System.out.println(jour);
-        }
-
-
 
    }
 
