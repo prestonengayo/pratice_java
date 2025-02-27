e10ba8b6a5d6cf119ae570546f7180e28c0a830b
gloire <gloirengayo@gmail.com>
2025-02-27 20:12:23 +0100

    jeu devinette

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,156 @@ public class Main {
 
 
     public static void main(String[] args) {
-        // tableau
 
-        int[][] nomber = {{2,4,6}, {1,3,5} };
+        int numberMagic1 = (int) (Math.random() * 10) + 1 ;
+        int numberMagic2 = (int) (Math.random() * 100) + 1;
+        int numberMagic3 = (int) (Math.random() * 1000) + 1;
+        int numberMagic4 = (int) (Math.random() * 10000) + 1;
+        int numberLevel = 0;
+        int numberDev = 0;
+        String choice;
 
-        System.out.println(nomber[0][2]);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println(nomber.length);
+        System.out.println("Selectionnez le niveau : 1,2,3 ou 4");
+        System.out.println("1 : niveau facile");
+        System.out.println("2 : niveau intermédiaire");
+        System.out.println("3 : niveau moyen");
+        System.out.println("4 : niveau diffile");
+        numberLevel = scanner.nextInt();
 
-        for (int i = 0 ; i < nomber.length; i++){
-            for (int j = 0; j < nomber[i].length; j++){
-                System.out.println("L'enfant se trouvant à la position n°"+(j+1) + " du tableau "+(i+1)+" est : "+nomber[i][j]);
-            }
-        }
+       while (true){
+           if (numberLevel == 1){
+               System.out.println("Devinez un nombre compris en 1 et 10 : ");
+               numberDev = scanner.nextInt();
+               scanner.nextLine();
 
-   }
+               if (numberDev < numberMagic1){
+                   System.out.println("Le nombre entrer est trop petit");
+               } else if (numberDev > numberMagic1) {
+                   System.out.println("Le nombre entrer est trop grand");
+               } else if (numberDev == numberMagic1){
+                   System.out.println("Félicitations !!! Vous avez gagnés");
+
+                   System.out.println("Voulez-vous continuer : Oui ou Non ?");
+                   choice = scanner.nextLine();
+
+                       if (choice.equalsIgnoreCase("oui")){
+                           System.out.println("1 : niveau facile");
+                           System.out.println("2 : niveau intermédiaire");
+                           System.out.println("3 : niveau moyen");
+                           System.out.println("4 : niveau diffile");
+                           numberLevel = scanner.nextInt();
+                       } else if (choice.equalsIgnoreCase("non")){
+                           System.out.println("A très bientôt");
+                           break;
+                       } else {
+                           System.out.println("Réponse invalide");
+                           break;
+                       }
+               } else {
+                   System.out.println("Le nombre entrer est invalide");
+               }
+
+           } else if (numberLevel == 2) {
+               System.out.println("Devinez un nombre compris en 1 et 100 : ");
+               numberDev = scanner.nextInt();
+               scanner.nextLine();
+
+               if (numberDev < numberMagic2){
+                   System.out.println("Le nombre entrer est trop petit");
+               } else if (numberDev > numberMagic2) {
+                   System.out.println("Le nombre entrer est trop grand");
+               } else if (numberDev == numberMagic2){
+                   System.out.println("Félicitations !!! Vous avez gagnés");
+
+                   System.out.println("Voulez-vous continuer : Oui ou Non ?");
+                   choice = scanner.nextLine();
+
+                   if (choice.equalsIgnoreCase("oui")){
+                       System.out.println("1 : niveau facile");
+                       System.out.println("2 : niveau intermédiaire");
+                       System.out.println("3 : niveau moyen");
+                       System.out.println("4 : niveau diffile");
+                       numberLevel = scanner.nextInt();
+                   } else if (choice.equalsIgnoreCase("non")){
+                       System.out.println("A très bientôt");
+                       break;
+                   } else {
+                       System.out.println("Réponse invalide");
+                       break;
+                   }
+               } else {
+                   System.out.println("Le nombre entrer est invalide");
+               }
+           } else if (numberLevel == 3) {
+               System.out.println("Devinez un nombre compris en 1 et 1000 : ");
+               numberDev = scanner.nextInt();
+               scanner.nextLine();
+
+               if (numberDev < numberMagic3){
+                   System.out.println("Le nombre entrer est trop petit");
+               } else if (numberDev > numberMagic3) {
+                   System.out.println("Le nombre entrer est trop grand");
+               } else if (numberDev == numberMagic3){
+                   System.out.println("Félicitations !!! Vous avez gagnés");
+
+                   System.out.println("Voulez-vous continuer : Oui ou Non ?");
+                   choice = scanner.nextLine();
+
+                   if (choice.equalsIgnoreCase("oui")){
+                       System.out.println("1 : niveau facile");
+                       System.out.println("2 : niveau intermédiaire");
+                       System.out.println("3 : niveau moyen");
+                       System.out.println("4 : niveau diffile");
+                       numberLevel = scanner.nextInt();
+                   } else if (choice.equalsIgnoreCase("non")){
+                       System.out.println("A très bientôt");
+                       break;
+                   } else {
+                       System.out.println("Réponse invalide");
+                       break;
+                   }
+               } else {
+                   System.out.println("Le nombre entrer est invalide");
+               }
+           } else if (numberLevel == 4) {
+               System.out.println("Devinez un nombre compris en 1 et 10000 : ");
+               numberDev = scanner.nextInt();
+               scanner.nextLine();
+
+               if (numberDev < numberMagic4){
+                   System.out.println("Le nombre entrer est trop petit");
+               } else if (numberDev > numberMagic4) {
+                   System.out.println("Le nombre entrer est trop grand");
+               } else if (numberDev == numberMagic4){
+                   System.out.println("Félicitations !!! Vous avez gagnés");
+
+                   System.out.println("Voulez-vous continuer : Oui ou Non ?");
+                   choice = scanner.nextLine();
+
+                   if (choice.equalsIgnoreCase("oui")){
+                       System.out.println("1 : niveau facile");
+                       System.out.println("2 : niveau intermédiaire");
+                       System.out.println("3 : niveau moyen");
+                       System.out.println("4 : niveau diffile");
+                       numberLevel = scanner.nextInt();
+                   } else if (choice.equalsIgnoreCase("non")){
+                       System.out.println("A très bientôt");
+                       break;
+                   } else {
+                       System.out.println("Réponse invalide");
+                       break;
+                   }
+               } else {
+                   System.out.println("Le nombre entrer est invalide");
+               }
+           }
+
+       }
+
+    }
 
 }
+
+
