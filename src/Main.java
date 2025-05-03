904f1391e9531de07ff17740af740e482e2918b6
Prestone NGAYO <prestonengayo9@gmail.com>
2025-05-03 11:31:42 +0200

    Refactor Main class: simplified sum method, removed unused variables and methods, updated .gitignore to include .idea/stat.log

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,20 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-    static int age = 26;
+
 
     public static void main(String[] args) {
-
-        say_hello();
-        System.out.println(sum(2,3));
-        System.out.println(sum(2.5, 3.5));
-
-        System.out.println(age);
+        int result = sum(10);
+        System.out.println(result);
 
     }
-    
-    static void say_hello(){
-        System.out.println("Hello i'm in a function !");
-    }
 
-    static int sum(int nbre1, int nbre2){
-        return nbre1 + nbre2;
-    }
-
-
-    static double sum(double nbre1, double nbre2){
-        return nbre1 + nbre2;
+    public static int sum(int k) {
+        if (k > 0) {
+            return k + sum(k - 1);
+        } else {
+            return 0;
+        }
     }
 
 
